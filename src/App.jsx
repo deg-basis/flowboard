8f5ddc7efeb13b3ae5d0f48796b43714392cf540
David Goldfarb <deg@degel.com>
2024-06-13 15:00:03 +0300

    Add AntD and start app boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Layout, Menu } from "antd";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Header, Content } = Layout;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const menuItems = [
+  { key: "1", label: "Events" },
+  { key: "2", label: "Funnel" },
+  { key: "3", label: "Config" },
+  { key: "4", label: "Help" },
+];
+
+const App = () => (
+  <Layout style={{ height: "100vh" }}>
+    <Header className="header">
+      <div className="logo">
+        <img
+          src="/flowboard/BasisTech.png"
+          alt="BasisTech Logo"
+          style={{ height: "31px", margin: "16px 24px 16px 0" }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Menu mode="horizontal" defaultSelectedKeys={["1"]} items={menuItems} />
+    </Header>
+    <Content style={{ marginTop: 64, padding: "0 24px", background: "#fff" }}>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at
+        lacus nec felis feugiat cursus. Nulla facilisi. Integer at magna a orci
+        tincidunt fermentum sit amet ac nulla. Suspendisse potenti. Mauris
+        placerat neque et sapien volutpat, ut fringilla ipsum bibendum. Nunc non
+        urna vitae est vehicula pellentesque. Vivamus vel metus id arcu
+        ullamcorper lacinia a ac mauris. Quisque tempor, ex et tincidunt
+        scelerisque, nisl arcu vehicula enim, sit amet varius felis ipsum non
+        metus.
       </p>
-    </>
-  )
-}
+    </Content>
+  </Layout>
+);
 
-export default App
+export default App;
