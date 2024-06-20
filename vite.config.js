cf4f2673efc2c08f099f379ecb0553b386086b86
David Goldfarb <deg@degel.com>
2024-06-20 12:53:58 +0300

    Chunk Vite dependency modules for faster updates

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,25 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/flowboard/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Place dependencies in separate module to speedup updates
+          if (id.includes("node_modules")) {
+            if (id.includes("/@ant-design") || id.includes("/antd")) {
+              return "antd";
+            }
+            if (id.includes("/rc-") || id.includes("/@rc-")) {
+              return "rc";
+            }
+            return "vendor";
+          } else {
+            return "main";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 700, // kbytes, before gzip
+  },
 });
